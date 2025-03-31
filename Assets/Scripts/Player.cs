6c2a3ad33a1b456a558c451222d0a8ff01939aa4
YoungMoo Kim <rladuan612@gmail.com>
2025-03-31 18:09:40 +0900

    create player & enemies & waypoints

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Player : MonoBehaviour
+{   
+    [SerializeField] float moveSpeed = 3f;
+    [SerializeField] float paddingLeft; 
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
+    Vector2 rawInput;
+    Vector2 minBounds; 
+    Vector2 maxBounds;
+
+    void Start()
+    {
+        InitBounds();
+    } 
+    void Update()
+    {
+      PlayerMove();
+    }
+
+    void PlayerMove() // 플레이어 이동
+    {
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight); // Clamp 지정된 범위를 넘지 않게 막아주는 함수
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+        transform.position = newPos;
+    }
+    void OnMove(InputValue value) //플레이어 조작
+    {
+        rawInput = value.Get<Vector2>();
+    }
+
+    void InitBounds() 
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0,0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1,1));
+    }
+}
